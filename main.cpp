cf999280f40b84c2bd7ee114b9b6b740f636a14e
siddimore <siddimore@gmail.com>
2024-07-24 09:14:19 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,18 +3,18 @@
 
 int main() {
     // Set Memtable Size
-    LSMTree lsmTree(3);
+    SSTableManager  ssm(3);
 
-    lsmTree.insert("key1", "val1");
-    lsmTree.insert("key2", "val2");
-    lsmTree.insert("key3", "val3");
+    ssm.insert("key1", "val1");
+    ssm.insert("key2", "val2");
+    ssm.insert("key3", "val3");
     // Trigger Flush
-    lsmTree.insert("key4", "val4");
+    ssm.insert("key4", "val4");
 
-    std::cout << "Query key1: " << lsmTree.query("key1") << std::endl;
-    std::cout << "Query key3: " << lsmTree.query("key3") << std::endl;
-    std::cout << "Query key4: " << lsmTree.query("key4") << std::endl;
-    std::cout << "Query key5: " << lsmTree.query("key5") << std::endl; // Should return empty
+    std::cout << "Query key1: " << ssm.query("key1") << std::endl;
+    std::cout << "Query key3: " << ssm.query("key3") << std::endl;
+    std::cout << "Query key4: " << ssm.query("key4") << std::endl;
+    std::cout << "Query key5: " << ssm.query("key5") << std::endl; // Should return empty
 
     return 0;
 
